4dcc1c359585c18d74c5a6a6058ad15d562c7beb
Tyler Hunt <tyhunt04@gmail.com>
2019-06-12 17:12:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ class Customer {
   constructor(name, neighborhood) {
     this.name = name;
     this.id = ++customerId;
-    this.neighborhoodId = neighborhood.id;
+    this.neighborhoodId = neighborhood;
 
     store.customers.push(this);
   };
