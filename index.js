0fc002c99124b73006020c8140008039fd7df087
Tyler Hunt <tyhunt04@gmail.com>
2019-06-12 16:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
 
 class Neighborhood {
-  
+
   constructor(name) {
     this.name = name;
     this.id = ++neighborhoodId;
   };
-  
+
 }
