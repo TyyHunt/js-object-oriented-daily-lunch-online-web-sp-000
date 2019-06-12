47732ee1db65607a32477a4199e0c93dba9475af
Tyler Hunt <tyhunt04@gmail.com>
2019-06-12 16:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -83,3 +83,30 @@ class Customer {
   };
 
 };
+
+class Delivery {
+
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+
+    store.neighborhoods.push(this);
+  };
+
+  deliveries() {
+    return store.deliveries.filter(
+      function(delivery) {
+        return delivery.neighborhoodId = this.id;
+      }.bind(this)
+    )
+  };
+
+  customers() {
+    return store.costomers.filter(
+      function(customer) {
+        return customer.neighborhoodId = this.id;
+      }.bind(this)
+    )
+  };
+
+};
